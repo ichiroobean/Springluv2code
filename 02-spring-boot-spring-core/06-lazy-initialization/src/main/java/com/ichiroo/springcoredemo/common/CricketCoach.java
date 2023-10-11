13758054c32ea695c7f3b7ef2bc5dccaca963a28
ichiroobean <62367629+ichiroobean@users.noreply.github.com>
2023-10-11 14:30:02 +0700

    Add files via upload

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/ichiroo/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.ichiroo.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 14 minutes lesson 3";
+    }
+}
