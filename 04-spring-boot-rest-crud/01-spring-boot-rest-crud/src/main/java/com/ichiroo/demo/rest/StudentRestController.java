b80f3c95e05d7e155164c10551479f0e854c571e
ichiroobean <ichiroobean@gmail.com>
2023-10-24 17:29:50 +0700

    add @ControllerAdvice

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ichiroo/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ichiroo/demo/rest/StudentRestController.java
@@ -46,29 +46,5 @@ public class StudentRestController {
     }
 
     // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse>handlerException(StudentNotFoundException exc){
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        //return ResponseEntity
 
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    //add another exception handler  ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse>handlerException(Exception e){
-
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(e.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        //return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
