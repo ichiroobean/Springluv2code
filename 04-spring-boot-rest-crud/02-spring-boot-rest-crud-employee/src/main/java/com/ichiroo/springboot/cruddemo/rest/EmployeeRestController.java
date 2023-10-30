c1d1ef38785762d6b61d36e394af279b734882f0
Ichiroo <ichiroobean@gmail.com>
2023-10-31 00:08:41 +0700

    add create, update, delete  rest method

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ichiroo/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ichiroo/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.ichiroo.springboot.cruddemo.rest;
 import com.ichiroo.springboot.cruddemo.dao.EmployeeDAO;
 import com.ichiroo.springboot.cruddemo.entity.Employee;
 import com.ichiroo.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +17,8 @@ public class EmployeeRestController {
         employeeService=theEmployeeService;
     }
     //expose "/employee" and return a list of employee
-    @GetMapping("/employee")
-    public List<Employee>finAll(){
+    @GetMapping("/employees")
+    public List<Employee>findAll(){
         return employeeService.findAll();
     }
 
@@ -30,7 +27,41 @@ public class EmployeeRestController {
     public Employee getEmployee(@PathVariable int employeeId){
         Employee theEmployee = employeeService.findById(employeeId);
         if (theEmployee == null){
-            throw  new RuntimeException("");
+            throw  new RuntimeException("Employee id not found - "+employeeId);
         }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        //also just in case they pass an id in JSON...set id to 0
+        //this is to force a save of new item...instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    //add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee =employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} -delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        //throw exception if null
+        if (tempEmployee==null){
+            throw new RuntimeException("Employee id not found - "+employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id - "+ employeeId;
     }
 }
